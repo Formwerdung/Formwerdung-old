03680ababbab824da9f48091df75f34e914f36d4
Marin Aeschbach <marin.aeschbach@gmail.com>
2016-08-31 20:51:49 +0200

    Project images

--- assemblefile.js
+++ assemblefile.js
@@ -129,56 +129,26 @@ app.task('scripts', () => {
 
 // Task: Images
 app.task('images', () => {
-  return app.src('source/*.{jpg,png,gif}')
-    // .pipe($.responsiveImages({
-    //   'content/*.{png,jpg,gif}': [{
-    //     width: 360,
-    //     suffix: '-xs'
-    //   }, {
-    //     width: 672,
-    //     suffix: '-sm'
-    //   }, {
-    //     width: 928,
-    //     suffix: '-md'
-    //   }, {
-    //     width: 1258,
-    //     suffix: '-lg'
-    //   }, {
-    //     width: 2516,
-    //     suffix: '-2x'
-    //   }],
-    //   'mobile/*.{png,jpg,gif}': [{
-    //     width: 320
-    //   }, {
-    //     width: 640,
-    //     suffix: '-2x'
-    //   }],
-    //   'tablet/*.{png,jpg,gif}': [{
-    //     width: 768
-    //   }, {
-    //     width: 1536,
-    //     suffix: '-2x'
-    //   }],
-    //   'desktop/*.{png,jpg,gif}': [{
-    //     width: 1024
-    //   }, {
-    //     width: 2048,
-    //     suffix: '-2x'
-    //   }],
-    //   'fullscreen/*.{png,jpg,gif}': [{
-    //     width: 544,
-    //     suffix: '-xs'
-    //   }, {
-    //     width: 768,
-    //     suffix: '-sm'
-    //   }, {
-    //     width: 992,
-    //     suffix: '-md'
-    //   }, {
-    //     width: 1920
-    //   }]
-    // }))
-    // .pipe($.flatten())
+  return app.src('source/*/*.{jpg,png,gif}')
+    .pipe($.responsiveImages({
+      'content/*.{png,jpg,gif}': [{
+        width: 360,
+        suffix: '-xs'
+      }, {
+        width: 672,
+        suffix: '-sm'
+      }, {
+        width: 928,
+        suffix: '-md'
+      }, {
+        width: 1258,
+        suffix: '-lg'
+      }, {
+        width: 2048,
+        suffix: '-2x'
+      }]
+    }))
+    .pipe($.flatten())
     .pipe($.cache($.imagemin({
       progressive: true,
       interlaced: true,
